30ee7d785157578ff88e0154d55d38d9a51d6205
wycode.cn <wangyu0503@gmail.com>
2016-12-31 00:49:46 +0800

    add map

--- app/src/main/java/cn/wycode/bike/MapFragment.java
+++ app/src/main/java/cn/wycode/bike/MapFragment.java
@@ -3,9 +3,12 @@ package cn.wycode.bike;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.amap.api.maps.MapView;
 
@@ -13,7 +16,7 @@ import com.amap.api.maps.MapView;
  * 地图
  * Created by wy on 2016/12/22.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment{
     private MapView mapView;
 
     public MapFragment() {
@@ -27,6 +30,7 @@ public class MapFragment extends Fragment {
         mapView.onCreate(savedInstanceState);
         return rootView;
     }
+
 
     @Override
     public void onDestroy() {
