ef2ac27e4b8b28b69af70ecf3242a5136345d5da
王郁 <www_wayne_001@163.com>
2017-01-10 17:42:44 +0800

    Update HelpFragment.java

--- app/src/main/java/cn/wycode/bike/activity/fragement/HelpFragment.java
+++ app/src/main/java/cn/wycode/bike/activity/fragement/HelpFragment.java
@@ -1,17 +1,17 @@
 package cn.wycode.bike.activity.fragement;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import cn.wycode.bike.activity.AboutActivity;
 import cn.wycode.bike.R;
+import cn.wycode.bike.activity.HelpActivity;
+import cn.wycode.bike.activity.QuestionListActivity;
+import cn.wycode.bike.activity.ServiceTypeActivity;
 
 /**
  * 帮助
@@ -20,8 +20,20 @@ import cn.wycode.bike.R;
 
 public class HelpFragment extends BaseFragment implements View.OnClickListener {
 
+    @BindView(R.id.ll_qa)
+    LinearLayout llQA;
+
     @BindView(R.id.ll_about)
     LinearLayout llAbout;
+
+    @BindView(R.id.ll_guide)
+    LinearLayout llGuide;
+
+    @BindView(R.id.ll_service)
+    LinearLayout llService;
+
+    @BindView(R.id.ll_tel)
+    LinearLayout llTel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,7 +43,7 @@ public class HelpFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
     protected void initView() {
-        setOnClickListeners(this, llAbout);
+        setOnClickListeners(this, llAbout,llGuide,llQA,llService,llTel);
     }
 
     @Override
@@ -43,8 +55,25 @@ public class HelpFragment extends BaseFragment implements View.OnClickListener {
     public void onClick(View v) {
         Intent intent = new Intent();
         switch (v.getId()) {
+            case R.id.ll_tel:
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("tel://965318"));
+                break;
+            case R.id.ll_qa:
+                intent.setClass(getContext(), QuestionListActivity.class);
+                break;
             case R.id.ll_about:
                 intent.setClass(getContext(), AboutActivity.class);
+                break;
+            case R.id.ll_service:
+                intent.setClass(getContext(), ServiceTypeActivity.class);
+                break;
+            case R.id.ll_guide:
+                intent.setClass(getContext(), HelpActivity.class);
+                intent.putExtra(HelpActivity.TITLE_KEY,"办卡指南");
+                intent.putExtra(HelpActivity.HEAD_KEY,getString(R.string.guide_head));
+                intent.putExtra(HelpActivity.BODY_KEY,getString(R.string.guide_body));
+                break;
         }
         startActivity(intent);
     }
