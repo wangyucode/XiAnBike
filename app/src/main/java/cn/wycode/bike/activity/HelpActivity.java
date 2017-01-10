a5ebe37c9a13993a9e022b80eb15f793f8333ab2
王郁 <www_wayne_001@163.com>
2017-01-10 17:47:08 +0800

    Update HelpActivity.java

--- app/src/main/java/cn/wycode/bike/activity/HelpActivity.java
+++ app/src/main/java/cn/wycode/bike/activity/HelpActivity.java
@@ -1,7 +1,10 @@
 package cn.wycode.bike.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import cn.wycode.bike.BaseActivity;
 import cn.wycode.bike.R;
 
@@ -12,14 +15,34 @@ import cn.wycode.bike.R;
 
 public class HelpActivity extends BaseActivity {
 
+    public static final String TITLE_KEY = "TITLE_KEY";
+    public static final String HEAD_KEY = "HEAD_KEY";
+    public static final String BODY_KEY = "BODY_KEY";
+
+    private String title, head, body;
+
+    @BindView(R.id.tv_head)
+    TextView tvHead;
+
+    @BindView(R.id.tv_body)
+    TextView tvBody;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentViewWithDefaultTitle(R.layout.activity_help,"帮助");
+        title = getIntent().getStringExtra(TITLE_KEY);
+        head = getIntent().getStringExtra(HEAD_KEY);
+        body = getIntent().getStringExtra(BODY_KEY);
+        setContentViewWithDefaultTitle(R.layout.activity_help, title == null ? "帮助" : title);
     }
 
     @Override
     protected void initView() {
-
+        if (!TextUtils.isEmpty(head)) {
+            tvHead.setText(head);
+        }
+        if (!TextUtils.isEmpty(body)) {
+            tvBody.setText(body);
+        }
     }
 }
