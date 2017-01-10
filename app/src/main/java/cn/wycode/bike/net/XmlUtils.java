e04c762337248537a9572999ac998020c71e5dda
王郁 <www_wayne_001@163.com>
2017-01-10 17:38:42 +0800

    Update XmlUtils.java

--- app/src/main/java/cn/wycode/bike/net/XmlUtils.java
+++ app/src/main/java/cn/wycode/bike/net/XmlUtils.java
@@ -1,11 +1,6 @@
 package cn.wycode.bike.net;
 
-import android.util.Xml;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import org.xmlpull.v1.XmlPullParser;
+import com.alibaba.fastjson.JSON;
 
 import java.util.List;
 
@@ -17,16 +12,15 @@ import cn.wycode.bike.model.BikeStation;
  */
 
 public class XmlUtils {
-    public static String getJsonString(String response){
+    public static String getJsonString(String response) {
         int start = response.indexOf("<ns1:out>");
-        int end =  response.indexOf("</ns1:out>");
+        int end = response.indexOf("</ns1:out>");
 
-        return response.substring(start+9,end);
+        return response.substring(start + 9, end);
     }
 
-    public static List<BikeStation> parserJson(String json){
-        Gson gson  = new Gson();
-        return gson.fromJson(json, new TypeToken<List<BikeStation>>(){}.getType());
+    public static List<BikeStation> parserJson(String json) {
+        return JSON.parseArray(json, BikeStation.class);
     }
 
 
